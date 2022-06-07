c90514bb5030d2fb5979a4eacbcf41e92cdf101e
Ariel Monti <djaroldito@gmail.com>
2022-06-07 13:10:02 -0300

    homework resuelto

--- 01-IntroToCS/homework/homework.js
+++ 01-IntroToCS/homework/homework.js
@@ -3,11 +3,25 @@
 function BinarioADecimal(num) {
   // tu codigo aca
 
+    let sum = 0;
+
+    for (let i = 0; i < num.length; i++) {
+    sum = sum + num[i] * Math.pow(2, num.length -1 -i);
+    }
+    return sum;
+
 }
 
 function DecimalABinario(num) {
   // tu codigo aca
-
+  if (num === 0) return "0";
+  var resultado = [];
+  while (num>=1){
+      resultado.unshift(num % 2);
+      num = Math.floor(num / 2);
+      
+  }
+ return resultado.join('');
 }
 
 
@@ -15,3 +29,4 @@ module.exports = {
   BinarioADecimal,
   DecimalABinario,
 }
+
